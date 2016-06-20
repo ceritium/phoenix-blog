281c4750fefc02cbd8acde98fba9a261c5175ec4
Jose Galisteo <ceritium@gmail.com>
2016-06-20 20:45:59 +0200

    alias for session controller

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,12 +1,13 @@
 defmodule Blog.SessionController do
   use Blog.Web, :controller
+  alias Blog.Auth
 
   def new(conn, _) do
     render conn, "new.html"
   end
 
   def create(conn, %{"session" => %{"email" => email, "password" => pass}}) do
-    case Blog.Auth.login_by_username_and_pass(conn, email, pass, repo: Repo) do
+    case Auth.login_by_username_and_pass(conn, email, pass, repo: Repo) do
       {:ok, conn} ->
         conn
         |> put_flash(:info, "Welcome back!")
@@ -20,7 +21,7 @@ defmodule Blog.SessionController do
 
   def delete(conn, _) do
     conn
-    |> Blog.Auth.logout()
+    |> Auth.logout()
     |> redirect(to: post_path(conn, :index))
   end
 end
